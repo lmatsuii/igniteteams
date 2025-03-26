565250a9203c08af47042314d58fc76ab4e3f403
lmatsuii <leonardomatsuii@gmail.com>
2025-03-25 23:48:05 -0300

    feat: Import components to screen

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,14 @@
 import { StyleSheet, Text, View } from "react-native";
 import { Container } from "./styles";
 import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
+import { GroupCard } from "@components/GroupCard";
 export function Groups() {
   return (
     <Container>
       <Header></Header>
+      <Highlight title="Turmas" subtitle="jogue com a sua turma"></Highlight>
+      <GroupCard title="galera do ignite"></GroupCard>
     </Container>
   );
 }
