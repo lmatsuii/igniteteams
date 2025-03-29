d07b92304bd2384c9c5eeb18a78f0cebf9597ecf
lmatsuii <leonardomatsuii@gmail.com>
2025-03-29 09:41:37 -0300

    feat: Add components in Groups screen

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -3,12 +3,23 @@ import { Container } from "./styles";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
+import { useState } from "react";
+import { FlatList } from "react-native";
+import { ListEmpty } from "@components/ListEmpty";
+
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([]);
   return (
     <Container>
       <Header></Header>
       <Highlight title="Turmas" subtitle="jogue com a sua turma"></Highlight>
-      <GroupCard title="galera do ignite"></GroupCard>
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && {flex: 1}}
+        ListEmptyComponent={() => (<ListEmpty message="Que tal cadastrar a primeira turma?"></ListEmpty>)}
+      ></FlatList>
     </Container>
   );
 }
