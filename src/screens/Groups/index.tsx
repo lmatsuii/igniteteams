fc2ac4b71ffce83624921eacbd7dd45e5d000293
lmatsuii <leonardomatsuii@gmail.com>
2025-03-29 09:55:49 -0300

    feat: Add button component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -6,6 +6,7 @@ import { GroupCard } from "@components/GroupCard";
 import { useState } from "react";
 import { FlatList } from "react-native";
 import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Buttons";
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
@@ -20,6 +21,9 @@ export function Groups() {
         contentContainerStyle={groups.length === 0 && {flex: 1}}
         ListEmptyComponent={() => (<ListEmpty message="Que tal cadastrar a primeira turma?"></ListEmpty>)}
       ></FlatList>
+      <Button
+      title="Criar nova turma"
+      ></Button>
     </Container>
   );
 }
