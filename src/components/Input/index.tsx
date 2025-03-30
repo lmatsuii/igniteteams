16ec260819922c5bbf2ab77d1261666c77588f34
lmatsuii <leonardomatsuii@gmail.com>
2025-03-30 09:42:40 -0300

    feat: Add component Input

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,9 @@
+import { Container } from "./styles";
+import { TextInputProps } from "react-native";
+
+export function Input({...rest}: TextInputProps) {
+    return (
+        <Container
+        {...rest}></Container>
+    )
+}
