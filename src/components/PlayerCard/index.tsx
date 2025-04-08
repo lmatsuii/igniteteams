6133a635f6cb0a6582f46e7a49cb6a4f924d5f44
lmatsuii <leonardomatsuii@gmail.com>
2025-04-07 23:48:39 -0300

    feat: Add PlayerCard filter

---
+++ src/components/PlayerCard/index.tsx
@@ -0,0 +1,17 @@
+import { ButtonIcon } from "@components/ButtonIcon";
+import { Container, Name, Icon } from "./styles";
+
+type Props = {
+    name: string;
+    onRemove: () => void;
+}
+
+export function PlayerCard({name, onRemove}:Props) {
+    return(
+        <Container>
+            <Icon name='person'></Icon>
+            <Name>{name}</Name>
+            <ButtonIcon icon="close" type='SECONDARY' onPress={onRemove}></ButtonIcon>
+        </Container>
+    )
+}
